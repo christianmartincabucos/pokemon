80e720479fd7441b82baf1f14417b54cbb7e787d
Christian Martin Cabucos <christianmartincabucos@gmail.com>
2022-10-23 22:46:31 +0800

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    // app/Http/Controllers/AuthController.php
+
+    private $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function login(Request $request)
+    {
+        if (!auth()->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Invalid login details'
+                ], 401);
+            }
+
+        $user = $this->user->where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    private $user;
+    private $request;
+    public function __construct(User $user, Request $request)
+    {
+        $this->user = $user;
+        $this->request = $request;
+    }   
+
+    public function register()
+    {
+        $validatedData = $this->request->validate([
+            'first_name'    => 'required|string|max:255',
+            'last_name'     => 'required|string|max:255',
+            'birthday'      => 'required|string|max:255|date_format:Y-m-d',
+            'email'         => 'required|string|email|max:255|unique:users',
+            'password'      => 'required|string|min:8',
+        ]);
+            $user = $this->user->create([
+                'first_name'=> $validatedData['first_name'],
+                'last_name' => $validatedData['last_name'],
+                'birthday'  => $validatedData['birthday'],
+                'email'     => $validatedData['email'],
+                'password'  => Hash::make($validatedData['password']),
+            ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+}
